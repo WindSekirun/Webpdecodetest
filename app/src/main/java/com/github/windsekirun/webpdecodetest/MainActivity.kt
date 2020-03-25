f603d7a3f7def16086ad22076c6bed1fddde38ba
Pyxis <Pyxis@uzuki.live>
2020-03-25 17:12:13 +0900

    fix index.html

--- app/src/main/java/com/github/windsekirun/webpdecodetest/MainActivity.kt
+++ app/src/main/java/com/github/windsekirun/webpdecodetest/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.github.windsekirun.webpdecodetest
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.integration.webp.decoder.WebpDrawable
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +25,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadWebpImage(imageView: ImageView, imageUrl: String) {
+        Glide.with(imageView.context)
+            .load(imageUrl)
+            .into(object: CustomTarget<Drawable>() {
+                override fun onLoadCleared(placeholder: Drawable?) {
 
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable,
+                    transition: Transition<in Drawable>?
+                ) {
+                    if (resource !is WebpDrawable) {
+                        imageView.setImageDrawable(resource)
+                        return
+                    }
+
+                    Log.d("WebpDecodeTest", "imageUrl $imageUrl loopCount = ${resource.loopCount}")
+
+                    resource.start()
+                    imageView.setImageDrawable(resource)
+                }
+            })
     }
 }
